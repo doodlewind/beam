90c90e3ce1a9d853ee7b2d67f698bea39dd6040c
Yifeng Wang <i@ewind.us>
2019-07-26 11:52:03 +0800

    refactor: simplify resource type defs

--- src/resources.js
+++ src/resources.js
@@ -3,12 +3,13 @@ import * as glUtils from './utils/gl-utils.js'
 
 export const createResource = (gl, type, state) => {
   const Types = ResourceTypes
-  class Resource { constructor (state) { this.state = state } }
+  class Resource {
+    constructor (state) { this.state = state; this.type = type }
+  }
 
   class DataBuffersResource extends Resource {
     constructor (state) {
       super(state)
-      this.type = Types.DataBuffers
       this.buffers = glUtils.initDataBuffers(gl, state)
     }
   }
@@ -16,7 +17,6 @@ export const createResource = (gl, type, state) => {
   class IndexBufferResource extends Resource {
     constructor (state) {
       super(state)
-      this.type = Types.IndexBuffer
       const { offset = 0, count = state.array.length } = state
       this.offset = offset
       this.count = count
@@ -25,11 +25,6 @@ export const createResource = (gl, type, state) => {
   }
 
   class UniformsResource extends Resource {
-    constructor (state) {
-      super(state)
-      this.type = Types.Uniforms
-    }
-
     set (key, val) {
       this.state[key] = val
       return this
@@ -39,7 +34,6 @@ export const createResource = (gl, type, state) => {
   class TexturesResource extends Resource {
     constructor (state) {
       super(state)
-      this.type = Types.Textures
       this.textures = glUtils.initTextures(gl, state)
     }
   }
