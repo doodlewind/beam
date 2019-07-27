aa8edffa68e958cec3d5de0dee8c20a37431b7da
Yifeng Wang <i@ewind.us>
2019-07-27 10:46:27 +0800

    feat: add multi filters example

---
+++ gallery/pages/image-processing/multi-filters.js
@@ -0,0 +1,50 @@
+import { Beam, ResourceTypes } from '../../../src/index.js'
+import {
+  BrightnessContrast, HueSaturation, Vignette
+} from '../../plugins/image-filter-plugins.js'
+import { createRect } from '../../utils/graphics-utils.js'
+import { loadImages } from '../../utils/image-loader.js'
+const { DataBuffers, IndexBuffer, Textures, Uniforms } = ResourceTypes
+
+const canvas = document.getElementById('gl-canvas')
+const beam = new Beam(canvas)
+
+const brightnessContrast = beam.plugin(BrightnessContrast)
+const hueSaturation = beam.plugin(HueSaturation)
+const vignette = beam.plugin(Vignette)
+
+const rect = createRect()
+const dataResource = beam.resource(DataBuffers, rect.data)
+const indexResource = beam.resource(IndexBuffer, rect.index)
+const argsResource = beam.resource(Uniforms, {})
+let imageResource
+
+const base = '../../assets/images/'
+const updateImage = name => loadImages(base + name).then(([image]) => {
+  const aspectRatio = image.naturalWidth / image.naturalHeight
+  canvas.height = 400
+  canvas.width = 400 * aspectRatio
+  imageResource = beam.resource(Textures, { img: { image, flip: true } })
+})
+
+const render = () => {
+  const resources = [dataResource, indexResource, argsResource, imageResource]
+  const plugin = brightnessContrast || hueSaturation || vignette // FIXME
+  beam.clear().draw(plugin, ...resources)
+}
+
+updateImage('ivan.jpg').then(render)
+
+const $imageSelect = document.getElementById('image-select')
+$imageSelect.addEventListener('change', () => {
+  updateImage($imageSelect.value).then(render)
+})
+
+const fields = ['brightness', 'contrast', 'hue', 'saturation', 'vignette']
+fields.forEach(field => {
+  const $field = document.getElementById(field)
+  $field.addEventListener('input', () => {
+    argsResource.set(field, $field.value)
+    render()
+  })
+})
