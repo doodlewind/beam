d6e1afa2da4e9524e68b5b4a6f80dad2709c6e3a
Yifeng Wang <i@ewind.us>
2019-07-29 11:58:31 +0800

    fix: ball mesh naming

--- gallery/pages/design-patterns/build-renderer.js
+++ gallery/pages/design-patterns/build-renderer.js
@@ -5,7 +5,7 @@ const canvas = document.querySelector('canvas')
 const renderer = new MeshRenderer(canvas)
 // renderer.wireframe = false // Try commenting this out!
 
-const ball = renderer.BoxMesh()
+const ball = renderer.BallMesh()
 renderer.add(ball)
-renderer.setCamera([10, 10, 10])
+renderer.setCamera([5, 5, 5])
 renderer.render()
